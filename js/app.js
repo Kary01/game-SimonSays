c91ad93e52b1b68a04208a58a87aaabc6cb7ba62
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-02-01 19:52:44 -0600

    obteniendo input

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ class Juego {
 
     // atributos básicos del juego
     inicializar(){
+        this.elegirColor = this.elegirColor.bind(this); //podemos identificar cual botón ha sido presionado
         btnEmpezar.classList.add('hide'); // oculta el botón de inicio
         this.nivel = 4; // indica el nivel del juego
         this.colores = { // creamos objetos para tenr un mejor control de los elementos
@@ -33,6 +34,7 @@ class Juego {
     // cada que se pase al siguiente nivel, iluminará una secuencia
     siguienteNivel(){
         this.iluminarSecuencia();
+        this.agregarEventoClick()
     }; 
 
     //pasamos los número de la secuencia a colores
@@ -67,6 +69,20 @@ class Juego {
     apagarColor(COLOR){
         this.colores[COLOR].classList.remove('light')
     }
+
+    // selección de color al hacer click
+    agregarEventoClick(){
+        this.colores.celeste.addEventListener('click', this.elegirColor);
+        this.colores.naranja.addEventListener('click', this.elegirColor);
+        this.colores.amarillo.addEventListener('click', this.elegirColor);
+        this.colores.verde.addEventListener('click', this.elegirColor);
+    };
+
+    //
+    elegirColor(ev){
+        console.log(this);
+    };
+
 };
 
 // al seleccionar el botón de inicio crea el elemento de clase Juego
